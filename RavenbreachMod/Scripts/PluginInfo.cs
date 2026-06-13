041b19c3beb47a56086438424beac615d9d987d1
Mum3n8r <lelandgm09@gmail.com>
2026-06-12 18:23:47 -0600

    Delete RavenbreachMod directory

--- RavenbreachMod/Scripts/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RavenbreachMod
-{
-    internal static class PluginInfo
-    {
-        internal const string PLUGIN_GUID    = "com.ravenbreach.mod";
-        internal const string PLUGIN_NAME    = "Ravenbreach";
-        internal const string PLUGIN_VERSION = "0.1.0";
-    }
-}
